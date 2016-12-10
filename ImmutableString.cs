ad8e91e061c2a1112c0fb5b9025463a7f40bceec
Nathan Connors <nathanaeljconnors@gmail.com>
2016-12-10 11:39:42 -0800

    immutable string practice complete

--- ImmutableString.cs
+++ ImmutableString.cs
@@ -6,7 +6,17 @@ namespace ImmutableString
 	{
 		static void Main(string[] args)
 		{
-			Console.Write("this works");
+			// Initialize a string, it is actually instantiated an object
+			String s = "This is my string";
+			
+			// Appears to overwrite string but is actually crating a new object
+			s = "new string";
+			Console.WriteLine(s);
+			
+			// Again creates a new object
+			s += " is immutable";
+			Console.WriteLine(s);
+
 		}
 	}
 	
