dc67803103cd0a5e67f5e1c4db6cb54d4f791b13
Nathan Connors <nathanaeljconnors@gmail.com>
2016-12-10 11:21:05 -0800

    one file on same page start

---
+++ OneFileClass.cs
@@ -0,0 +1,16 @@
+/*
+* A program that illustrates ability to put class on same pagel
+* Uses command line compile.
+*/
+using System;
+
+namespace OneFileClass
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("This test works!");
+		}
+	}
+}
